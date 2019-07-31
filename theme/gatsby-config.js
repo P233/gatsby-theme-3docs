cd1e6c864d96b0c26065c30c797544248ef67d5a
Peiwen Lu <hi@peiwen.lu>
2019-07-31 18:47:53 +0800

    Add gatsby remark plugins

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -8,7 +8,27 @@ module.exports = (options = {}) => {
           defaultLayouts: {
             docs: require.resolve("./src/layouts/doc.js"),
             default: require.resolve("./src/layouts/page.js")
-          }
+          },
+          gatsbyRemarkPlugins: [
+            {
+              resolve: `gatsby-remark-relative-images`
+            },
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                maxWidth: 900,
+                quality: 100
+              }
+            },
+            `gatsby-remark-prismjs`,
+            `gatsby-remark-external-links`,
+            {
+              resolve: `gatsby-remark-autolink-headers`,
+              options: {
+                icon: ``
+              }
+            }
+          ]
         }
       },
       {
