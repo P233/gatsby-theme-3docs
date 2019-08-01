8b5b77a98b587dd3ae0010134e1912a3ccdfa539
Peiwen Lu <hi@peiwen.lu>
2019-08-01 13:00:07 +0800

    Add example

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  siteMetadata: {
+    github: "https://github.com/P233/gatsby-theme-3docs",
+    twitter: "https://twitter.com/PeiwenLu"
+  },
   plugins: [{ resolve: `gatsby-theme-3docs`, options: {} }]
 };
