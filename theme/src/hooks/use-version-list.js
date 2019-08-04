f72d62b19e2f8e7ee20a3b71733f75630fba0ac1
Peiwen Lu <hi@peiwen.lu>
2019-08-04 18:21:39 +0800

    Sort the return list from  useVersionlist hook

--- theme/src/hooks/use-version-list.js
+++ theme/src/hooks/use-version-list.js
@@ -1,29 +1,36 @@
 import { useStaticQuery, graphql } from "gatsby";
 
-export default () => {
-  const { allMdx } = useStaticQuery(
-    graphql`
-      {
-        allMdx(
-          sort: { fields: frontmatter___chapterStringIndex, order: ASC }
-          filter: { fields: { sourceInstanceName: { eq: "docs" } } }
-        ) {
-          group(field: fields___version, limit: 1) {
-            edges {
-              node {
-                fields {
-                  slug
-                  version
-                }
+export default (isAscOrder = false) => {
+  const { allMdx } = useStaticQuery(graphql`
+    {
+      allMdx(
+        filter: { fields: { sourceInstanceName: { eq: "docs" } } }
+        sort: { fields: frontmatter___chapterStringIndex, order: ASC }
+      ) {
+        group(field: fields___version, limit: 1) {
+          edges {
+            node {
+              fields {
+                slug
+                version
               }
             }
           }
         }
       }
-    `
-  );
-  return allMdx.group.map(i => ({
-    version: i.edges[0].node.fields.version,
-    initPage: i.edges[0].node.fields.slug
-  }));
+    }
+  `);
+
+  return allMdx.group
+    .map(({ edges }) => ({
+      version: edges[0].node.fields.version,
+      initPage: edges[0].node.fields.slug
+    }))
+    .sort((a, b) => {
+      if (isAscOrder) {
+        return a.version.localeCompare(b.version);
+      } else {
+        return b.version.localeCompare(a.version);
+      }
+    });
 };
