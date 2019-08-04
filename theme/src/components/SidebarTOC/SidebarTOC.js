9d9230fba2a0c5a0e0ad83217543f5ac9be027ae
Peiwen Lu <hi@peiwen.lu>
2019-08-04 18:22:39 +0800

    Add useDocsToc hook

--- theme/src/components/SidebarTOC/SidebarTOC.js
+++ theme/src/components/SidebarTOC/SidebarTOC.js
@@ -1,57 +1,21 @@
 import React from "react";
 import { Link } from "gatsby";
 import classnames from "classnames";
-import { StaticQuery, graphql } from "gatsby";
-import useVersionList from "../../hooks/use-version-list.js";
-import IconChevronRight from "../../assets/svg/chevron-right.svg";
-import IconChevronDown from "../../assets/svg/chevron-down.svg";
+import useDocsToc from "hooks/use-docs-toc.js";
+import IconChevronDown from "assets/svg/chevron-down.svg";
+import IconChevronRight from "assets/svg/chevron-right.svg";
+import IconChevronsLeft from "assets/svg/chevrons-left.svg";
 import styles from "./SidebarTOC.module.scss";
 
-const createTocArray = rawData => {
-  let toc = [];
+export default props => {
+  const tocArray = useDocsToc(props.currentVersion);
 
-  rawData.forEach(({ node }) => {
-    let currentGroup = toc;
-    node.fields.parents.forEach(groupTitle => {
-      const lastEntry = currentGroup[currentGroup.length - 1];
-      if (!lastEntry || lastEntry.title !== groupTitle) {
-        currentGroup.push({
-          title: groupTitle,
-          list: []
-        });
-        currentGroup = currentGroup[currentGroup.length - 1].list;
-      } else {
-        currentGroup = lastEntry.list;
-      }
-    });
-
-    currentGroup.push({
-      title: node.frontmatter.title,
-      url: node.fields.slug
-    });
-  });
-
-  return toc;
-};
-
-const formatTitle = string => {
-  return string
-    .replace(/([a-z]|[A-Z]+)([A-Z])/g, "$1 $2")
-    .replace(/(\w+)(\d+)/g, "$1 $2")
-    .replace(/[-_]/g, " ");
-};
-
-const SidebarTOC = props => {
-  const rawDataList = props.data.allMdx.group;
-  const versionArray = useVersionList().reduce((a, c) => {
-    a.push(c.version);
-    return a;
-  }, []);
-  const targetVersionDataIndex = versionArray.indexOf(props.currentVersion);
-  if (targetVersionDataIndex === -1) return null;
-
-  const targetVersionRawData = rawDataList[targetVersionDataIndex].edges;
-  const tocArray = createTocArray(targetVersionRawData);
+  const formatTitle = string => {
+    return string
+      .replace(/([a-z]|[A-Z]+)([A-Z])/g, "$1 $2")
+      .replace(/(\w+)(\d+)/g, "$1 $2")
+      .replace(/[-_]/g, " ");
+  };
 
   const renderTOC = (list, title, isNested = false) => (
     <ul>
@@ -73,11 +37,13 @@ const SidebarTOC = props => {
                 [styles.nested]: isNested
               })}
             >
-              <IconChevronRight
-                className={classnames(styles.tocEntry__icon, {
-                  [styles.active]: props.currentPath === i.url
-                })}
-              />
+              {props.currentPath === i.url ? (
+                <IconChevronsLeft
+                  className={classnames(styles.tocEntry__icon, styles.active)}
+                />
+              ) : (
+                <IconChevronRight className={styles.tocEntry__icon} />
+              )}
               <span className={styles.tocEntry__body}>{i.title}</span>
             </Link>
           </li>
@@ -86,38 +52,5 @@ const SidebarTOC = props => {
     </ul>
   );
 
-  return (
-    <aside className={classnames(styles.sidebarTOC, props.className)}>
-      {renderTOC(tocArray)}
-    </aside>
-  );
+  return <aside className={styles.sidebarTOC}>{renderTOC(tocArray)}</aside>;
 };
-
-export default props => (
-  <StaticQuery
-    query={graphql`
-      query {
-        allMdx(
-          sort: { fields: frontmatter___chapterStringIndex, order: ASC }
-          filter: { fields: { sourceInstanceName: { eq: "docs" } } }
-        ) {
-          group(field: fields___version) {
-            edges {
-              node {
-                frontmatter {
-                  title
-                }
-                fields {
-                  slug
-                  version
-                  parents
-                }
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={data => <SidebarTOC data={data} {...props} />}
-  ></StaticQuery>
-);
---
+++ theme/src/hooks/use-docs-toc.js
@@ -0,0 +1,61 @@
+import { useStaticQuery, graphql } from "gatsby";
+
+const createTocArray = rawData => {
+  let toc = [];
+
+  rawData.forEach(({ node }) => {
+    let currentGroup = toc;
+    node.fields.parents.forEach(groupTitle => {
+      const lastEntry = currentGroup[currentGroup.length - 1];
+      if (!lastEntry || lastEntry.title !== groupTitle) {
+        currentGroup.push({
+          title: groupTitle,
+          list: []
+        });
+        currentGroup = currentGroup[currentGroup.length - 1].list;
+      } else {
+        currentGroup = lastEntry.list;
+      }
+    });
+
+    currentGroup.push({
+      title: node.frontmatter.title,
+      url: node.fields.slug
+    });
+  });
+
+  return toc;
+};
+
+export default targetVersion => {
+  const { allMdx } = useStaticQuery(graphql`
+    {
+      allMdx(
+        filter: { fields: { sourceInstanceName: { eq: "docs" } } }
+        sort: { fields: frontmatter___chapterStringIndex, order: ASC }
+      ) {
+        group(field: fields___version) {
+          edges {
+            node {
+              frontmatter {
+                title
+              }
+              fields {
+                slug
+                version
+                parents
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const versionList = allMdx.group.map(i => i.edges[0].node.fields.version);
+  const targetVersionIndex = versionList.indexOf(targetVersion);
+  if (targetVersionIndex === -1) return [];
+
+  const targetVersionRawData = allMdx.group[targetVersionIndex].edges;
+  return createTocArray(targetVersionRawData);
+};
