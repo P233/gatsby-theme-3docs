16211cf0686a338288bbe031b2f8a3bdda893811
Peiwen Lu <hi@peiwen.lu>
2019-08-04 11:28:36 +0800

    Add google font prefetch plugin

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -1,7 +1,16 @@
 const createMinifier = require("css-loader-minify-class");
 
 const defaultOptions = {
-  pageLayout: require.resolve("./src/layouts/PageLayout/index.js")
+  pageLayout: require.resolve("./src/layouts/PageLayout/index.js"),
+  googleFonts: [
+    {
+      family: "Noto Serif",
+      variants: ["400", "400i", "700"]
+    },
+    {
+      family: "Roboto"
+    }
+  ]
 };
 
 module.exports = options => {
@@ -15,7 +24,7 @@ module.exports = options => {
     },
     plugins: [
       {
-        resolve: `gatsby-plugin-mdx`,
+        resolve: "gatsby-plugin-mdx",
         options: {
           extensions: [".mdx", ".md"],
           defaultLayouts: {
@@ -23,73 +32,79 @@ module.exports = options => {
           },
           gatsbyRemarkPlugins: [
             {
-              resolve: `gatsby-remark-relative-images`
+              resolve: "gatsby-remark-relative-images"
             },
             {
-              resolve: `gatsby-remark-images`,
+              resolve: "gatsby-remark-images",
               options: {
                 maxWidth: 900,
                 quality: 100
               }
             },
-            `gatsby-remark-prismjs`,
-            `gatsby-remark-external-links`,
+            "gatsby-remark-prismjs",
+            "gatsby-remark-external-links",
             {
-              resolve: `gatsby-remark-autolink-headers`,
+              resolve: "gatsby-remark-autolink-headers",
               options: {
-                icon: ``
+                icon: ""
               }
             }
           ]
         }
       },
       {
-        resolve: `gatsby-source-filesystem`,
+        resolve: "gatsby-source-filesystem",
         options: {
-          name: `docs`,
-          path: `src/docs`
+          name: "docs",
+          path: "src/docs"
         }
       },
       {
-        resolve: `gatsby-source-filesystem`,
+        resolve: "gatsby-source-filesystem",
         options: {
-          name: `pages`,
-          path: `src/pages`
+          name: "pages",
+          path: "src/pages"
         }
       },
       {
-        resolve: `gatsby-plugin-sass`,
+        resolve: "gatsby-plugin-sass",
         options: {
           cssLoaderOptions:
-            process.env.NODE_ENV === `production`
+            process.env.NODE_ENV === "production"
               ? {
                   minimize: true,
                   getLocalIdent: createMinifier()
                 }
               : {
-                  localIdentName: `[local]`
+                  localIdentName: "[local]"
                 }
         }
       },
       {
-        resolve: `gatsby-plugin-react-svg`
+        resolve: "gatsby-plugin-react-svg"
       },
       {
-        resolve: `gatsby-plugin-react-helmet`
+        resolve: "gatsby-plugin-react-helmet"
       },
       {
-        resolve: `gatsby-plugin-manifest`,
+        resolve: "gatsby-plugin-prefetch-google-fonts",
         options: {
-          name: `gatsby-theme-3docs`,
-          short_name: `3docs`,
-          start_url: `/`,
-          background_color: `#663399`,
-          theme_color: `#663399`,
-          display: `minimal-ui`
+          fonts: options.googleFonts
         }
       },
       {
-        resolve: `gatsby-plugin-offline`
+        resolve: "gatsby-plugin-manifest",
+        options: {
+          name: "gatsby-theme-3docs",
+          short_name: "3docs",
+          start_url: "/",
+          background_color: "#663399",
+          theme_color: "#663399",
+          display: "minimal-ui"
+        }
+      },
+      {
+        resolve: "gatsby-plugin-offline"
       }
     ]
   };
