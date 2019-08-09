996756888bf723a07c8e9538add1fab4e4147523
Peiwen Lu <hi@peiwen.lu>
2019-08-09 08:13:30 +0800

    Export components and hooks

--- theme/index.js
+++ theme/index.js
@@ -1,1 +1,14 @@
-export * from "./src";
+// Components
+export { default as Root } from "layouts/Root";
+export { default as ContentTOC } from "components/ContentTOC";
+export { default as DocsTOC } from "components/DocsTOC";
+export { default as Header } from "components/Header";
+export { default as MDXContanier } from "components/MDXContainer";
+export { default as SEO } from "components/SEO";
+export { default as ThemeToggle } from "components/ThemeToggle";
+
+// Hooks
+export { default as useDocsTOC } from "hooks/use-docs-toc.js";
+export { default as useHeaderNavList } from "hooks/use-header-nav-list.js";
+export { default as useSiteMetadata } from "hooks/use-site-metadata.js";
+export { default as useVersionList } from "hooks/use-version-list.js";
